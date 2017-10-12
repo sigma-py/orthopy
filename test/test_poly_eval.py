65631451b52fe74ab2e33949c57b11eae9d34117
Nico Schlömer <nico.schloemer@gmail.com>
2017-10-12 23:35:33 +0200

    shorten polynomial evaluation test

--- test/test_poly_eval.py
+++ test/test_poly_eval.py
@@ -3,35 +3,26 @@
 from __future__ import division, print_function
 
 import orthopy
+import pytest
 import sympy
 
 
-def test_jacobi():
-    # alpha, beta = orthopy.recurrence_coefficients.jacobi(5, 0, 0)
-    alpha, beta = orthopy.recurrence_coefficients.legendre(0)
+@pytest.mark.parametrize('n, val1, val2', [
+    (0, 1, 1),
+    (1, sympy.Rational(1, 2), 1),
+    # Not normalized! (normalized: 23/256, 1)
+    (5, sympy.Rational(23, 2016), sympy.Rational(8, 63)),
+    ]
+    )
+def test_legendre(n, val1, val2):
+    alpha, beta = orthopy.recurrence_coefficients.legendre(n)
     x = sympy.Rational(1, 2)
     val = orthopy.tools.evaluate_orthogonal_polynomial(alpha, beta, x)
-    assert val == 1
+    assert val == val1
     val = orthopy.tools.evaluate_orthogonal_polynomial(alpha, beta, 1)
-    assert val == 1
-
-    alpha, beta = orthopy.recurrence_coefficients.legendre(1)
-    x = sympy.Rational(1, 2)
-    val = orthopy.tools.evaluate_orthogonal_polynomial(alpha, beta, x)
-    assert val == sympy.Rational(1, 2)
-    val = orthopy.tools.evaluate_orthogonal_polynomial(alpha, beta, 1)
-    assert val == 1
-
-    alpha, beta = orthopy.recurrence_coefficients.legendre(5)
-    # Not normalized! (normalized: 23/256)
-    x = sympy.Rational(1, 2)
-    val = orthopy.tools.evaluate_orthogonal_polynomial(alpha, beta, x)
-    assert val == sympy.Rational(23, 2016)
-    # Not normalized! (normalized: 1)
-    val = orthopy.tools.evaluate_orthogonal_polynomial(alpha, beta, 1)
-    assert val == sympy.Rational(8, 63)
+    assert val == val2
     return
 
 
 if __name__ == '__main__':
-    test_jacobi()
+    test_legendre(0, 1, 1)
