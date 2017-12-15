159540ea47ef93f83e2e3c7fde289ff10c2cba60
Nico Schlömer <nico.schloemer@gmail.com>
2017-12-15 17:06:03 +0100

    quad: fix for orth_tree

--- orthopy/quadrilateral/orth.py
+++ orthopy/quadrilateral/orth.py
@@ -20,24 +20,22 @@ def orth_tree(n, X, symbolic=True):
         (0, 2)   (1, 2)   (2, 2)
          ...      ...      ...
     '''
-    frac = sympy.Rational if symbolic else lambda x, y: x/y
-    sqrt = sympy.sqrt if symbolic else numpy.sqrt
-
     p0, a, b, c = legendre(n+1, 'normal', symbolic=symbolic)
 
-    p0 *= sqrt(frac(1, 2))
+    p0 **= 2
     out = [numpy.array([numpy.full(X.shape[1:], p0)])]
     for L in range(1, n+1):
         out.append(numpy.concatenate([
             # The order of X and a is important here. If X is int and a is
             # sympy, then the product will be sympy for X*a and float for a*X.
-            out[L-1] * (X[0] * a[L] - b[L]),
-            [out[L-1][-1] * (X[1] * a[L] - b[L])],
+            [out[L-1][0] * (X[0] * a[L-1] - b[L-1])],
+            out[L-1][1:] * (X[0] * a[L-1] - b[L-1]),
+            [out[L-1][-1] * (X[1] * a[L-1] - b[L-1])],
             ])
             )
 
         if L > 1:
-            out[-1][:L-1] -= out[L-2] * c[L]
-            out[-1][-1] -= out[L-2][L-2] * c[L]
+            out[-1][:L-1] -= out[L-2] * c[L-1]
+            out[-1][-1] -= out[L-2][L-2] * c[L-1]
 
     return out
