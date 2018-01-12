7b59919b7722e8c2b9cf39fc50a1ecb37a526096
Nico Schlömer <nico.schloemer@gmail.com>
2018-01-12 22:44:48 +0100

    move a bunch of methods to quadpy
    (All the quadrature-related stuff.)

--- orthopy/line/__init__.py
+++ orthopy/line/__init__.py
@@ -4,6 +4,4 @@
 from . import recurrence_coefficients
 from . import schemes
 from .alp import *
-from .main import *
-from .poly_classes import *
 from .tools import *
--- orthopy/line/tools.py
+++ orthopy/line/tools.py
@@ -8,44 +8,6 @@ from . import recurrence_coefficients
 from ..tools import line_tree
 
 
-def coefficients_from_gauss(points, weights):
-    '''Given the points and weights of a Gaussian quadrature rule, this method
-    reconstructs the recurrence coefficients alpha, beta as appearing in the
-    tridiagonal Jacobi matrix tri(b, a, b).
-    This is using "Method 2--orthogonal reduction" from (section 3.2 in [4]).
-    The complexity is O(n^3); a faster method is suggested in 3.3 in [4].
-    '''
-    n = len(points)
-    assert n == len(weights)
-
-    flt = numpy.vectorize(float)
-    points = flt(points)
-    weights = flt(weights)
-
-    A = numpy.zeros((n+1, n+1))
-
-    # In sytrd, the _last_ row/column of Q are e, so put the values there.
-    a00 = 1.0
-    A[n, n] = a00
-    k = numpy.arange(n)
-    A[k, k] = points
-    A[n, :-1] = numpy.sqrt(weights)
-    A[:-1, n] = numpy.sqrt(weights)
-
-    # Implemented in
-    # <https://github.com/scipy/scipy/issues/7775>
-    sytrd, sytrd_lwork = get_lapack_funcs(('sytrd', 'sytrd_lwork'))
-
-    # query lwork (optional)
-    lwork, info = sytrd_lwork(n+1)
-    assert info == 0
-
-    _, d, e, _, info = sytrd(A, lwork=lwork)
-    assert info == 0
-
-    return d[:-1][::-1], e[::-1]**2
-
-
 def clenshaw(a, alpha, beta, t):
     '''Clenshaw's algorithm for evaluating
 
@@ -108,39 +70,6 @@ def evaluate_orthogonal_polynomial(t, p0, a, b, c):
         vals0, vals1 = vals1, vals2
         vals2 = vals1 * (t*a_k - b_k) - vals0 * c_k
     return vals2
-
-
-def check_coefficients(moments, alpha, beta):
-    '''In his article [3], Walter Gautschi suggests a method for checking if a
-    Gauss quadrature rule is sane. This method implements test #3 for the
-    article.
-    '''
-    n = len(alpha)
-    assert len(beta) == n
-
-    D = numpy.empty(n+1)
-    Dp = numpy.empty(n+1)
-    D[0] = 1.0
-    D[1] = moments[0]
-    Dp[0] = 0.0
-    Dp[1] = moments[1]
-    for k in range(2, n+1):
-        A = numpy.array([moments[i:i+k] for i in range(k)])
-        D[k] = numpy.linalg.det(A)
-        #
-        A[:, -1] = moments[k:2*k]
-        Dp[k] = numpy.linalg.det(A)
-
-    errors_alpha = numpy.zeros(n)
-    errors_beta = numpy.zeros(n)
-
-    errors_alpha[0] = abs(alpha[0] - (Dp[1]/D[1] - Dp[0]/D[0]))
-    errors_beta[0] = abs(beta[0] - D[1])
-    for k in range(1, n):
-        errors_alpha[k] = abs(alpha[k] - (Dp[k+1]/D[k+1] - Dp[k]/D[k]))
-        errors_beta[k] = abs(beta[k] - D[k+1]*D[k-1]/D[k]**2)
-
-    return errors_alpha, errors_beta
 
 
 def show(*args, **kwargs):
