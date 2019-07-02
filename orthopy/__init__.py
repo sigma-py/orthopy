02b7642d67d72c2f5e3e034436efea604e88f77f
Nico Schlömer <nico.schloemer@gmail.com>
2019-07-02 21:24:28 +0200

    remove pipdate

--- orthopy/__init__.py
+++ orthopy/__init__.py
@@ -41,11 +41,3 @@ __all__ = [
     "sphere",
     "triangle",
 ]
-
-try:
-    import pipdate
-except ImportError:
-    pass
-else:
-    if pipdate.needs_checking(__name__):
-        print(pipdate.check(__name__, __version__), end="")
