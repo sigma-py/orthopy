86a1168eed3ff082a89aa731119f3f1e44ee911d
Nico Schlömer <nico.schloemer@gmail.com>
2020-05-18 22:27:23 +0200

    format

--- test/test_e3r2.py
+++ test/test_e3r2.py
@@ -11,15 +11,26 @@ def test_integral0(n=4):
     x = sympy.Symbol("x")
     y = sympy.Symbol("y")
     z = sympy.Symbol("z")
-    vals = numpy.concatenate(orthopy.e3r2.tree(numpy.array([x, y, z]), n, symbolic=True))
+    vals = numpy.concatenate(
+        orthopy.e3r2.tree(numpy.array([x, y, z]), n, symbolic=True)
+    )
 
-    assert sympy.integrate(
-        vals[0] * sympy.exp(-(x ** 2) - y ** 2 - z ** 2), (x, -oo, +oo), (y, -oo, +oo), (z, -oo, +oo)
-    ) == sympy.sqrt(sympy.sqrt(sympy.pi)) ** 3
+    assert (
+        sympy.integrate(
+            vals[0] * sympy.exp(-(x ** 2) - y ** 2 - z ** 2),
+            (x, -oo, +oo),
+            (y, -oo, +oo),
+            (z, -oo, +oo),
+        )
+        == sympy.sqrt(sympy.sqrt(sympy.pi)) ** 3
+    )
     for val in vals[1:]:
         assert (
             sympy.integrate(
-                val * sympy.exp(-x ** 2 - y ** 2 - z ** 2), (x, -oo, +oo), (y, -oo, +oo), (z, -oo, +oo)
+                val * sympy.exp(-(x ** 2) - y ** 2 - z ** 2),
+                (x, -oo, +oo),
+                (y, -oo, +oo),
+                (z, -oo, +oo),
             )
             == 0
         )
@@ -29,13 +40,18 @@ def test_orthogonality(n=4):
     x = sympy.Symbol("x")
     y = sympy.Symbol("y")
     z = sympy.Symbol("z")
-    tree = numpy.concatenate(orthopy.e3r2.tree(numpy.array([x, y, z]), n, symbolic=True))
+    tree = numpy.concatenate(
+        orthopy.e3r2.tree(numpy.array([x, y, z]), n, symbolic=True)
+    )
     vals = tree * numpy.roll(tree, 1, axis=0)
 
     for val in vals:
         assert (
             sympy.integrate(
-                val * sympy.exp(-x ** 2 - y ** 2 - z ** 2), (x, -oo, +oo), (y, -oo, +oo), (z, -oo, +oo)
+                val * sympy.exp(-(x ** 2) - y ** 2 - z ** 2),
+                (x, -oo, +oo),
+                (y, -oo, +oo),
+                (z, -oo, +oo),
             )
             == 0
         )
@@ -45,12 +61,17 @@ def test_normality(n=4):
     x = sympy.Symbol("x")
     y = sympy.Symbol("y")
     z = sympy.Symbol("z")
-    tree = numpy.concatenate(orthopy.e3r2.tree(numpy.array([x, y, z]), n, symbolic=True))
+    tree = numpy.concatenate(
+        orthopy.e3r2.tree(numpy.array([x, y, z]), n, symbolic=True)
+    )
 
     for val in tree:
         assert (
             sympy.integrate(
-                val ** 2 * sympy.exp(-x ** 2 - y ** 2 - z ** 2), (x, -oo, +oo), (y, -oo, +oo), (z, -oo, +oo)
+                val ** 2 * sympy.exp(-(x ** 2) - y ** 2 - z ** 2),
+                (x, -oo, +oo),
+                (y, -oo, +oo),
+                (z, -oo, +oo),
             )
             == 1
         )
