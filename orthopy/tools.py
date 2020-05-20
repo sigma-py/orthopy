2351c2bc2a666c5f4abe59cbe3c58a738b4e96e6
Nico Schlömer <nico.schloemer@gmail.com>
2020-05-20 20:24:54 +0200

    remove line-evaluate

--- orthopy/tools.py
+++ orthopy/tools.py
@@ -142,17 +142,3 @@ class ProductIterator:
         self.last[0] = out
         self.k += 1
         return out
-
-
-def line_evaluate(t, p0, a, b, c):
-    """Evaluate the orthogonal polynomial defined by its recurrence coefficients
-    a, b, and c at the point(s) t.
-    """
-    vals1 = numpy.zeros_like(t, dtype=int)
-    # The order is important here; see <https://github.com/sympy/sympy/issues/13637>.
-    vals2 = numpy.ones_like(t) * p0
-
-    for a_k, b_k, c_k in zip(a, b, c):
-        vals0, vals1 = vals1, vals2
-        vals2 = vals1 * (t * a_k - b_k) - vals0 * c_k
-    return vals2
