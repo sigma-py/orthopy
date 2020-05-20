902f2cbc29f478dfe2a6e7a41defb173deef12fe
Nico Schlömer <nico.schloemer@gmail.com>
2020-05-20 14:17:11 +0200

    more test simplification

--- test/test_enr2.py
+++ test/test_enr2.py
@@ -5,79 +5,47 @@ from sympy import oo
 import orthopy
 
 
+def _integrate3(f, x, y, z):
+    return sympy.integrate(
+        f * sympy.exp(-(x ** 2) - y ** 2 - z ** 2),
+        (x, -oo, +oo),
+        (y, -oo, +oo),
+        (z, -oo, +oo),
+    )
+
+
 def test_integral0(n=4):
     """Make sure that the polynomials are orthonormal
     """
-    x = sympy.Symbol("x")
-    y = sympy.Symbol("y")
-    z = sympy.Symbol("z")
+    x, y, z = sympy.symbols("x, y, z")
     vals = numpy.concatenate(
         orthopy.enr2.tree(numpy.array([x, y, z]), n, symbolic=True)
     )
 
-    assert (
-        sympy.integrate(
-            vals[0] * sympy.exp(-(x ** 2) - y ** 2 - z ** 2),
-            (x, -oo, +oo),
-            (y, -oo, +oo),
-            (z, -oo, +oo),
-        )
-        == sympy.sqrt(sympy.sqrt(sympy.pi)) ** 3
-    )
+    assert _integrate3(vals[0], x, y, z) == sympy.sqrt(sympy.sqrt(sympy.pi)) ** 3
     for val in vals[1:]:
-        assert (
-            sympy.integrate(
-                val * sympy.exp(-(x ** 2) - y ** 2 - z ** 2),
-                (x, -oo, +oo),
-                (y, -oo, +oo),
-                (z, -oo, +oo),
-            )
-            == 0
-        )
-    return
+        assert _integrate3(val, x, y, z) == 0
 
 
 def test_orthogonality(n=4):
-    x = sympy.Symbol("x")
-    y = sympy.Symbol("y")
-    z = sympy.Symbol("z")
+    x, y, z = sympy.symbols("x, y, z")
     tree = numpy.concatenate(
         orthopy.enr2.tree(numpy.array([x, y, z]), n, symbolic=True)
     )
     vals = tree * numpy.roll(tree, 1, axis=0)
 
     for val in vals:
-        assert (
-            sympy.integrate(
-                val * sympy.exp(-(x ** 2) - y ** 2 - z ** 2),
-                (x, -oo, +oo),
-                (y, -oo, +oo),
-                (z, -oo, +oo),
-            )
-            == 0
-        )
-    return
+        assert _integrate3(val, x, y, z) == 0
 
 
 def test_normality(n=4):
-    x = sympy.Symbol("x")
-    y = sympy.Symbol("y")
-    z = sympy.Symbol("z")
-    tree = numpy.concatenate(
-        orthopy.enr2.tree(numpy.array([x, y, z]), n, symbolic=True)
-    )
+    xyz = numpy.array(sympy.symbols("x, y, z"))
 
-    for val in tree:
-        assert (
-            sympy.integrate(
-                val ** 2 * sympy.exp(-(x ** 2) - y ** 2 - z ** 2),
-                (x, -oo, +oo),
-                (y, -oo, +oo),
-                (z, -oo, +oo),
-            )
-            == 1
-        )
-    return
+    for k, level in enumerate(orthopy.enr2.Iterator(xyz, symbolic=True)):
+        for val in level:
+            assert _integrate3(val ** 2, *xyz) == 1
+        if k == n:
+            break
 
 
 if __name__ == "__main__":
