e48c662df017fe450686b233c28bcbf015c2fdc6
Nico Schlömer <nico.schloemer@gmail.com>
2020-05-26 14:54:12 +0200

    faster cn tests

--- test/test_cn.py
+++ test/test_cn.py
@@ -1,42 +1,67 @@
+import math
+import operator
+import sys
+from functools import reduce
+
 import numpy
 import sympy
 
 import orthopy
 
+# def _integrate(f, X):
+#     integration_limits = [(x, -1, +1) for x in X]
+#     return sympy.integrate(f, *integration_limits)
 
-def _integrate(f, X):
-    integration_limits = [(x, -1, +1) for x in X]
-    return sympy.integrate(f, *integration_limits)
+
+def _prod(iterable):
+    if sys.version < "3.8":
+        return reduce(operator.mul, iterable, 1)
+    return math.prod(iterable)
+
+
+def _integrate_monomial(exponents):
+    if any(k % 2 == 1 for k in exponents):
+        return 0
+    return _prod(sympy.Rational(2, k + 1) for k in exponents)
+
+
+def _integrate_poly(p):
+    return sum(c * _integrate_monomial(k) for c, k in zip(p.coeffs(), p.monoms()))
 
 
 def test_integral0(n=4, dim=5):
     """Make sure that the polynomials are orthonormal
     """
     X = [sympy.Symbol("x{}".format(k)) for k in range(dim)]
-    vals = numpy.concatenate(orthopy.cn.tree(n, X, symbolic=True))
+    p = [sympy.poly(x, X) for x in X]
+    vals = numpy.concatenate(orthopy.cn.tree(n, p, symbolic=True))
+    vals[0] = sympy.poly(vals[0], X)
 
-    assert _integrate(vals[0], X) == sympy.sqrt(2) ** dim
+    assert _integrate_poly(vals[0]) == sympy.sqrt(2) ** dim
     for val in vals[1:]:
-        assert _integrate(val, X) == 0
+        assert _integrate_poly(val) == 0
 
 
-def test_orthogonality(n=3, dim=5):
+def test_orthogonality(n=4, dim=5):
     X = [sympy.Symbol("x{}".format(k)) for k in range(dim)]
+    p = [sympy.poly(x, X) for x in X]
 
-    tree = numpy.concatenate(orthopy.cn.tree(n, X, symbolic=True))
+    tree = numpy.concatenate(orthopy.cn.tree(n, p, symbolic=True))
     vals = tree * numpy.roll(tree, 1, axis=0)
 
     for val in vals:
-        assert _integrate(val, X) == 0
+        assert _integrate_poly(val) == 0
 
 
-def test_normality(n=3, dim=5):
+def test_normality(n=4, dim=5):
     X = [sympy.Symbol("x{}".format(k)) for k in range(dim)]
+    p = [sympy.poly(x, X) for x in X]
 
-    tree = numpy.concatenate(orthopy.cn.tree(n, X, symbolic=True))
+    vals = numpy.concatenate(orthopy.cn.tree(n, p, symbolic=True))
+    vals[0] = sympy.poly(vals[0], X)
 
-    for val in tree:
-        assert _integrate(val ** 2, X) == 1
+    for val in vals:
+        assert _integrate_poly(val ** 2) == 1
 
 
 if __name__ == "__main__":
