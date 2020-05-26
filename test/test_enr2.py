bb43ac4e394061558cdcd1d929742b56f9afb8c6
Nico Schlömer <nico.schloemer@gmail.com>
2020-05-26 17:28:47 +0200

    much faster enr2 tests

--- test/test_enr2.py
+++ test/test_enr2.py
@@ -2,52 +2,69 @@ import itertools
 
 import numpy
 import sympy
-from sympy import oo
+from sympy import sqrt, pi, Rational
 
 import orthopy
 
 standardization = "physicist"
 
 
-def _integrate(f, X):
-    ranges = [(x, -oo, +oo) for x in X]
-    return sympy.integrate(f * sympy.exp(-(sum(x ** 2 for x in X))), *ranges)
+# def _integrate(f, X):
+#     ranges = [(x, -oo, +oo) for x in X]
+#     return sympy.integrate(f * sympy.exp(-(sum(x ** 2 for x in X))), *ranges)
 
 
-# def _integrate_monomial(exponents):
-#     if any(k % 2 == 1 for k in exponents):
-#         return 0
-#     return prod(sympy.Rational(2, k + 1) for k in exponents)
-#
-#
-# def _integrate_poly(p):
-#     return sum(c * _integrate_monomial(k) for c, k in zip(p.coeffs(), p.monoms()))
+def _integrate_monomial(exponents):
+    if any(k % 2 == 1 for k in exponents):
+        return 0
+
+    if all(k == 0 for k in exponents):
+        n = len(exponents)
+        return sqrt(pi) ** n
+
+    # find first nonzero
+    idx = next(i for i, j in enumerate(exponents) if j > 0)
+    alpha = Rational(exponents[idx] - 1, 2)
+    k2 = exponents.copy()
+    k2[idx] -= 2
+    return _integrate_monomial(k2) * alpha
+
+
+def _integrate_poly(p):
+    return sum(c * _integrate_monomial(list(k)) for c, k in zip(p.coeffs(), p.monoms()))
 
 
 def test_integral0(n=3, d=5):
     X = [sympy.symbols("x{}".format(k)) for k in range(d)]
-    vals = numpy.concatenate(orthopy.enr2.tree(n, X, standardization, symbolic=True))
+    p = [sympy.poly(x, X) for x in X]
+    vals = numpy.concatenate(orthopy.enr2.tree(n, p, standardization, symbolic=True))
+    vals[0] = sympy.poly(vals[0], X)
 
-    assert _integrate(vals[0], X) == sympy.sqrt(sympy.sqrt(sympy.pi)) ** d
+    assert _integrate_poly(vals[0]) == sympy.sqrt(sympy.sqrt(sympy.pi)) ** d
     for val in vals[1:]:
-        assert _integrate(val, X) == 0
+        assert _integrate_poly(val) == 0
 
 
 def test_orthogonality(n=3, d=5):
     X = [sympy.symbols("x{}".format(k)) for k in range(d)]
-    tree = numpy.concatenate(orthopy.enr2.tree(n, X, standardization, symbolic=True))
+    p = [sympy.poly(x, X) for x in X]
+    tree = numpy.concatenate(orthopy.enr2.tree(n, p, standardization, symbolic=True))
     vals = tree * numpy.roll(tree, 1, axis=0)
 
     for val in vals:
-        assert _integrate(val, X) == 0
+        assert _integrate_poly(val) == 0
 
 
 def test_normality(n=3, d=5):
     X = [sympy.symbols("x{}".format(k)) for k in range(d)]
-    iterator = orthopy.enr2.Iterator(X, standardization, symbolic=True)
-    for level in itertools.islice(iterator, n):
+    p = [sympy.poly(x, X) for x in X]
+    iterator = orthopy.enr2.Iterator(p, standardization, symbolic=True)
+
+    for k, level in enumerate(itertools.islice(iterator, n)):
+        if k == 0:
+            level[0] = sympy.poly(level[0], X)
         for val in level:
-            assert _integrate(val ** 2, X) == 1
+            assert _integrate_poly(val ** 2) == 1
 
 
 if __name__ == "__main__":
