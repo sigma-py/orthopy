09d94e8fbeab1d8cd41df778d1ce9808e89b8eef
Nico Schlömer <nico.schloemer@gmail.com>
2020-05-26 17:41:12 +0200

    format

--- orthopy/enr2/orth.py
+++ orthopy/enr2/orth.py
@@ -10,7 +10,7 @@ def tree(n, *args, **kwargs):
 
 class Iterator(ProductIterator):
     def __init__(self, X, standardization, symbolic=False):
-        rc = {"probabilist": RCProbabilistNormal, "physicist": RCPhysicistNormal,}[
+        rc = {"probabilist": RCProbabilistNormal, "physicist": RCPhysicistNormal}[
             standardization
         ](symbolic)
         super().__init__(rc, X, symbolic)
