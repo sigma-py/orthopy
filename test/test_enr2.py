4476a4a490f3b2573b095bee1f87c050d355b33d
Nico Schlömer <nico.schloemer@gmail.com>
2020-05-26 17:21:42 +0200

    d=5 for enr2

--- test/test_enr2.py
+++ test/test_enr2.py
@@ -9,39 +9,45 @@ import orthopy
 standardization = "physicist"
 
 
-def _integrate3(f, x, y, z):
-    return sympy.integrate(
-        f * sympy.exp(-(x ** 2 + y ** 2 + z ** 2)),
-        (x, -oo, +oo),
-        (y, -oo, +oo),
-        (z, -oo, +oo),
-    )
+def _integrate(f, X):
+    ranges = [(x, -oo, +oo) for x in X]
+    return sympy.integrate(f * sympy.exp(-(sum(x ** 2 for x in X))), *ranges)
 
 
-def test_integral0(n=3):
-    X = sympy.symbols("x, y, z")
+# def _integrate_monomial(exponents):
+#     if any(k % 2 == 1 for k in exponents):
+#         return 0
+#     return prod(sympy.Rational(2, k + 1) for k in exponents)
+#
+#
+# def _integrate_poly(p):
+#     return sum(c * _integrate_monomial(k) for c, k in zip(p.coeffs(), p.monoms()))
+
+
+def test_integral0(n=3, d=5):
+    X = [sympy.symbols("x{}".format(k)) for k in range(d)]
     vals = numpy.concatenate(orthopy.enr2.tree(n, X, standardization, symbolic=True))
 
-    assert _integrate3(vals[0], *X) == sympy.sqrt(sympy.sqrt(sympy.pi)) ** 3
+    assert _integrate(vals[0], X) == sympy.sqrt(sympy.sqrt(sympy.pi)) ** d
     for val in vals[1:]:
-        assert _integrate3(val, *X) == 0
+        assert _integrate(val, X) == 0
 
 
-def test_orthogonality(n=3):
-    X = sympy.symbols("x, y, z")
+def test_orthogonality(n=3, d=5):
+    X = [sympy.symbols("x{}".format(k)) for k in range(d)]
     tree = numpy.concatenate(orthopy.enr2.tree(n, X, standardization, symbolic=True))
     vals = tree * numpy.roll(tree, 1, axis=0)
 
     for val in vals:
-        assert _integrate3(val, *X) == 0
+        assert _integrate(val, X) == 0
 
 
-def test_normality(n=3):
-    X = sympy.symbols("x, y, z")
+def test_normality(n=3, d=5):
+    X = [sympy.symbols("x{}".format(k)) for k in range(d)]
     iterator = orthopy.enr2.Iterator(X, standardization, symbolic=True)
     for level in itertools.islice(iterator, n):
         for val in level:
-            assert _integrate3(val ** 2, *X) == 1
+            assert _integrate(val ** 2, X) == 1
 
 
 if __name__ == "__main__":
