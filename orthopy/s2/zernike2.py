5129c8b6a2209118c5597b0e67d67fb4ef9b0c59
Nico Schlömer <nico.schloemer@gmail.com>
2020-06-06 11:37:35 +0200

    add zernike2

---
+++ orthopy/s2/zernike2.py
@@ -0,0 +1,53 @@
+import itertools
+
+import numpy
+
+
+def tree(n, *args, **kwargs):
+    return list(itertools.islice(Eval(*args, **kwargs), n + 1))
+
+
+class Eval:
+    """
+    Similar to
+
+    Torben B. Andersen,
+    Efficient and robust recurrence relations for the Zernike circle polynomials and
+    their derivatives in Cartesian coordinates,
+    Optics Express Vol. 26, Issue 15, pp. 18878-18896 (2018),
+    <https://doi.org/10.1364/OE.26.018878>
+
+    but a lot simpler. Can probably be generalized to n-ball.
+    """
+
+    def __init__(self, X, scaling, symbolic=False):
+        self.X = X
+        self.L = 0
+        self.last = [None, None]
+        self.p0 = 1
+
+    def __iter__(self):
+        return self
+
+    def __next__(self):
+        if self.L == 0:
+            out = numpy.array([0 * self.X[0] + self.p0])
+        else:
+            shape = list(self.last[0].shape)
+            shape[0] += 1
+            out = numpy.zeros(shape, dtype=self.last[0].dtype)
+
+            last_X = self.last[0] * self.X[0]
+            last_Y = self.last[0] * self.X[1]
+            last_Y = last_Y[::-1]
+
+            # The minus sign could go onto the other last_Y, too.
+            out[1:] += last_X + last_Y
+            out[:-1] += last_X - last_Y
+            if self.L > 1:
+                out[1:-1] -= self.last[1]
+
+        self.last[1] = self.last[0]
+        self.last[0] = out
+        self.L += 1
+        return out
